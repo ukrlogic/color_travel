57f451de5b4074d009e53405616bac4ea475ceaf
Vladow <vladow@yandex.ru>
2014-12-09 01:39:35 +0200

    fix filters

--- src/Application/UkrLogic/TourClientBundle/Service/AviaTours.php
+++ src/Application/UkrLogic/TourClientBundle/Service/AviaTours.php
@@ -23,9 +23,19 @@ class AviaTours
 
         $request->TourSearchRequest->dataLimit = $limit;
         $request->TourSearchRequest->dataOffset = ($page - 1) * $limit;
+        $cityId = null;
+        $countryId = null;
 
-        $request->TourSearchRequest->addChild('cityId', array_search(true, $options->get('cities')));
-        $request->TourSearchRequest->addChild('countryId', array_search(true, $options->get('countries')));
+        if (is_array($options->get('cities'))) {
+            $cityId = array_search(true, $options->get('cities')) ? : '668';
+        }
+
+        if (is_array($options->get('countries'))) {
+            $countryId = array_search(true, $options->get('countries')) ? : '12';
+        }
+
+        $request->TourSearchRequest->addChild('cityId', $cityId ? : '668');
+        $request->TourSearchRequest->addChild('countryId', $countryId ? : '12');
 
         if (null !== $options->get('days_from')) {
             $request->TourSearchRequest->addChild('durationFrom', $options->get('days_from'));
